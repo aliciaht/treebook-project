eaf2c193830e5551c41b69469a4de6dcd2d24ef9
Alicia Tan <aliciaht@gmail.com>
2013-05-11 22:46:50 -0400

    Add tests for user first, last, profile name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "a user should enter a first name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?
+	end
+
+	test "a user should enter a last name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+	test "a user should enter a profile name" do 
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "user should have unique proflile name" do
+		user = User.new
+		user.first_name = 'Alicia'
+		user.last_name = 'Tan'
+		user.email = 'aliciaht@gmail.com'
+		user.password = 'password'
+		user.password_confirmation = 'password'
+		user.profile_name = 'Alicia'
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "profile name should not have spaces" do
+		user = User.new
+		user.profile_name = "My  profilename"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("must be formatted correctly.")
+	end 
+
 end
